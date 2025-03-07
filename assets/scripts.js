cc445dd671f1c8aa6db1d44ca654e1220a707118
Mehdi <mehdi.lemaire.pro@gmail.com>
2025-03-07 14:34:18 +0100

    gallery cassé

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,15 @@
-if (document.querySelector('.gallery')) {
+$(document).ready(function() {
     $('.gallery').mauGallery({
-        columns: { xs: 1, sm: 2, md: 3, lg: 3, xl: 3 },
+        columns: {
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 3,
+            xl: 3
+        },
         lightBox: true,
         lightboxId: 'myAwesomeLightbox',
         showTags: true,
         tagsPosition: 'top'
     });
-}
+});
